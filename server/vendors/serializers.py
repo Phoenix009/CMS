7fcaa4fcf08f08a0a9a6d00167a9b9a31d15876e
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-13 23:10:08 +0530

    CRUD on nested serializers

--- server/vendors/serializers.py
+++ server/vendors/serializers.py
@@ -1,15 +1,54 @@
 from rest_framework import serializers
 from .models import Vendor, Gunmen
 
+
+class RelatedFieldAlternative(serializers.PrimaryKeyRelatedField):
+    def __init__(self, **kwargs):
+        self.serializer = kwargs.pop("serializer", None)
+        if self.serializer is not None and not issubclass(
+            self.serializer, serializers.Serializer
+        ):
+            raise TypeError('"serializer" is not a valid serializer class')
+        super().__init__(**kwargs)
+
+    def use_pk_only_optimization(self):
+        return False if self.serializer else True
+
+    def to_representation(self, instance):
+        if self.serializer:
+            return self.serializer(instance, context=self.context).data
+        return super().to_representation(instance)
+
+
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
-        fields = ['name', 'address','email','contact','officer_incharge', 'created_by', 'created_at']
+        fields = [
+            "name",
+            "address",
+            "email",
+            "contact",
+            "officer_incharge",
+            "created_by",
+            "created_at",
+        ]
 
 
 class GunmenSerializer(serializers.ModelSerializer):
-    vendor = VendorSerializer()
+    # vendor = VendorSerializer()
+    vendor = RelatedFieldAlternative(
+        queryset=Vendor.objects.all(), serializer=VendorSerializer
+    )
+
     class Meta:
         model = Gunmen
-        fields = ['first_name', 'last_name', 'vendor']
+        fields = ["first_name", "last_name", "vendor"]
 
+    # def create(self, validated_data):
+    #     vendor_data = validated_data.pop("vendor")
+    #     vendor_data = Vendor.objects.filter(vendor_data)
+    #     print(vendor_data)
+    #     gunman = Gunmen.objects.create(**validated_data)
+    #     # for track_data in tracks_data:
+    #     #     Track.objects.create(album=album, **track_data)
+    #     return gunman
