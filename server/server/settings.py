be0ef937f32ddbf884ee0f6faa2541bfdb4d195e
mustankap <mustankap@gmail.com>
2021-07-13 12:25:53 +0530

    changes

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-filters',
+    'django_filters',
     'users.apps.UsersConfig',
     'vendors.apps.VendorsConfig',
     'attendance.apps.AttendanceConfig',
