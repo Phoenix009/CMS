ff9ff091ce846a7d809a0db6297345960c33d357
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-13 21:44:44 +0530

    added CRUD for gunmen and vendors

--- server/vendors/urls.py
+++ server/vendors/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('gunmen/', views.GunmenList.as_view()),
-    path('vendor/', views.VendorList.as_view()),
+    path("gunmen/", views.GunmenList.as_view()),
+    path("gunmen/<int:pk>/", views.GunmenDetail.as_view()),
+    path("vendor/", views.VendorList.as_view()),
+    path("vendor/<int:pk>/", views.VendorDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- server/vendors/views.py
+++ server/vendors/views.py
@@ -19,16 +19,54 @@ class VendorList(
         return self.create(request, *args, **kwargs)
 
 
+class VendorDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
 class GunmenList(
     mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
 ):
     queryset = Gunmen.objects.all()
     serializer_class = GunmenSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["first_name", "last_name"]
+    filterset_fields = ["first_name", "last_name", "vendor"]
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class GunmenDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Gunmen.objects.all()
+    serializer_class = GunmenSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
