563cb4f7dd4fcdba02b5bf70da5b79e1497ab53f
Phoenix009 <jaideepmore307@gmail.com>
2021-07-14 16:34:00 +0530

    issue urls

--- server/attendance/urls.py
+++ server/attendance/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("attendance/", views.AttendanceList.as_view()),
     path("attendance/<int:pk>/", views.AttendanceDetail.as_view()),
+    path("issue/", views.IssueList.as_view()),
+    path("issue/<int:pk>/", views.IssueDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
