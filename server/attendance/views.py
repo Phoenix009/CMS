5516a5864c3a57adba0d3c1ef3d54b213f48d10d
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-19 20:21:34 +0530

    update models

--- server/attendance/views.py
+++ server/attendance/views.py
@@ -11,14 +11,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters
 from rest_framework.response import Response
-<<<<<<< HEAD
-
-# from vendors.models import Gunmen
-=======
 from datetime import datetime
 from vendors.models import Gunmen
 from users.models import User
->>>>>>> b4b10e5d4df72134348f9b3daf03058aa5552c01
+
 # from vendors.serializers import GunmenSerializer
 
 
@@ -65,8 +61,8 @@ class AttendanceList(
 
     def get(self, request, *args, **kwargs):
         params = request.query_params
-        start_date = params.get('start_date', datetime.min)
-        end_date = params.get('end_date', datetime.max)
+        start_date = params.get("start_date", datetime.min)
+        end_date = params.get("end_date", datetime.max)
 
         queryset = self.filter_queryset(self.get_queryset())
         queryset = queryset.filter(entry_time__date__range=[start_date, end_date])
@@ -79,51 +75,26 @@ class AttendanceList(
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-
     def post(self, request, *args, **kwargs):
-<<<<<<< HEAD
-        
-        new_attendance = AttendanceSerializer(data=request.data)
-        if not new_attendance.is_valid():
-            return Response(data='invalid request')
-
-        today = datetime.now()
-        attendance = Attendance.objects.filter(
-            entry_time__year=today.date().year,
-            entry_time__month=today.date().month,
-            entry_time__day=today.date().day,
-            gunmen=new_attendance.data["gunmen"]["id"],
-            branch=new_attendance.data["branch"]["id"],
-        ).first()
-
-        if attendance:
-            attendance.entry_time = today
-            attendance.exit_time = new_attendance.data.get("exit_time", attendance.exit_time)
-            attendance.save()
-            return Response(data=AttendanceSerializer(attendance).data)
-        else:
-            return self.create(request, *args, **kwargs)
-=======
         data = request.data
-        branch_id = data.get('branch')
-        added_by_id = data.get('added_by')
-        gunmen_ids = data.get('gunmen_id')
-        attendance_sheet = data.get('attendance_sheet')
-
+        branch_id = data.get("branch")
+        added_by_id = data.get("added_by")
+        gunmen_ids = data.get("gunmen_id")
+        attendance_sheet = data.get("attendance_sheet")
 
         result = []
 
         for gunmen_id in gunmen_ids:
             data = {
-                'gunmen': gunmen_id,
-                'branch':branch_id, 
-                'added_by':added_by_id, 
-                'attendance_sheet': attendance_sheet
+                "gunmen": gunmen_id,
+                "branch": branch_id,
+                "added_by": added_by_id,
+                "attendance_sheet": attendance_sheet,
             }
             print(data)
             new_attendance = AttendanceSerializer(data=data)
             if not new_attendance.is_valid():
-                return Response(data='invalid request')
+                return Response(data="invalid request")
 
             today = datetime.now()
             attendance = Attendance.objects.filter(
@@ -136,20 +107,28 @@ class AttendanceList(
 
             if attendance:
                 attendance.entry_time = today
-                attendance.exit_time = new_attendance.data.get("exit_time", attendance.exit_time)
+                attendance.exit_time = new_attendance.data.get(
+                    "exit_time", attendance.exit_time
+                )
                 attendance.save()
                 result.append(AttendanceSerializer(attendance).data)
             else:
-                gunmen_ = get_object_or_404(Gunmen, pk=data.pop('gunmen'))
-                added_by_ = get_object_or_404(User, pk=data.pop('added_by'))
-                branch_ = get_object_or_404(Branch, pk=data.pop('branch'))
-                attendance_sheet_ = get_object_or_404(AttendanceSheet, pk=data.pop('attendance_sheet'))
-                attendance = Attendance(gunmen=gunmen_, added_by=added_by_, branch=branch_, attendance_sheet=attendance_sheet_)
+                gunmen_ = get_object_or_404(Gunmen, pk=data.pop("gunmen"))
+                added_by_ = get_object_or_404(User, pk=data.pop("added_by"))
+                branch_ = get_object_or_404(Branch, pk=data.pop("branch"))
+                attendance_sheet_ = get_object_or_404(
+                    AttendanceSheet, pk=data.pop("attendance_sheet")
+                )
+                attendance = Attendance(
+                    gunmen=gunmen_,
+                    added_by=added_by_,
+                    branch=branch_,
+                    attendance_sheet=attendance_sheet_,
+                )
                 attendance.save()
                 result.append(AttendanceSerializer(attendance).data)
 
         return Response(data=result)
->>>>>>> b4b10e5d4df72134348f9b3daf03058aa5552c01
 
 
 class AttendanceDetail(
--- server/vendors/models.py
+++ server/vendors/models.py
@@ -9,28 +9,35 @@ class Vendor(models.Model):
     email = models.EmailField()
     contact = models.CharField(max_length=20)
     officer_incharge = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True
+    )
     created_at = models.DateTimeField(default=datetime.now)
-    
-    def __str__(self):
-        return f'{self.name} -> {self.email}'
 
- 
+    def __str__(self):
+        return f"{self.name} -> {self.email}"
+
+
 class Gunmen(models.Model):
+    GUNMEN_TYPES = (
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    )
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField()
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
 
 
 class Vehicle(models.Model):
+
     model_name = models.CharField(max_length=200)
     number_plate = models.CharField(max_length=200)
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f'{self.model_name} -> {self.number_plate}'
-
+        return f"{self.model_name} -> {self.number_plate}"
