8fd67aa6f2acebd8491e9b691274248b948436b8
mishrakeshav <47028747+mishrakeshav@users.noreply.github.com>
2021-07-20 21:49:54 +0530

    add leaflet

--- client/src/pages/VendorsAttendanceDetails/VendorsAttendanceDetails.js
+++ client/src/pages/VendorsAttendanceDetails/VendorsAttendanceDetails.js
@@ -32,28 +32,13 @@ import Label from "../../components/Label";
 import Scrollbar from "../../components/Scrollbar";
 import SearchNotFound from "../../components/SearchNotFound";
 import {
-	AppTasks,
-	AppNewUsers,
-	AppBugReports,
-	AppItemOrders,
-	AppNewsUpdate,
-	AppWeeklySales,
 	AppOrderTimeline,
-	AppCurrentVisits,
-	AppWebsiteVisits,
-	AppTrafficBySite,
-	AppCurrentSubject,
-	AppConversionRates,
 } from "../../components/_dashboard/app";
 import {
 	UserListHead,
-	UserListToolbar,
-	UserMoreMenu,
 } from "../../components/_dashboard/user";
 //
 import USERLIST from "../../_mocks_/user";
-import AddEmployee from '../../components/AddEmployee/AddEmployee';
-import { getAllEmployees } from "../../api/index";
 
 // ----------------------------------------------------------------------
 
@@ -67,147 +52,13 @@ const TABLE_HEAD = [
 	{ id: "" },
 ];
 
-// ----------------------------------------------------------------------
 
-function descendingComparator(a, b, orderBy) {
-	if (b[orderBy] < a[orderBy]) {
-		return -1;
-	}
-	if (b[orderBy] > a[orderBy]) {
-		return 1;
-	}
-	return 0;
-}
 
-function getComparator(order, orderBy) {
-	return order === "desc"
-		? (a, b) => descendingComparator(a, b, orderBy)
-		: (a, b) => -descendingComparator(a, b, orderBy);
-}
 
-function applySortFilter(array, comparator, query) {
-	const stabilizedThis = array.map((el, index) => [el, index]);
-	stabilizedThis.sort((a, b) => {
-		const order = comparator(a[0], b[0]);
-		if (order !== 0) return order;
-		return a[1] - b[1];
-	});
-	if (query) {
-		return filter(
-			array,
-			(_user) =>
-				_user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
-		);
-	}
-	return stabilizedThis.map((el) => el[0]);
-}
 
 export default function User() {
-	const [page, setPage] = useState(0);
-	const [order, setOrder] = useState("asc");
-	const [selected, setSelected] = useState([]);
-	const [orderBy, setOrderBy] = useState("name");
-	const [filterName, setFilterName] = useState("");
-	const [rowsPerPage, setRowsPerPage] = useState(5);
-	const [isAddEmployeeOpen, setAddEmployeeOpen] = useState(false);
-	const [employees, setEmployees] = useState([]);
-
 	
-	
-	// const handleClose
 
-	const handleRequestSort = (event, property) => {
-		const isAsc = orderBy === property && order === "asc";
-		setOrder(isAsc ? "desc" : "asc");
-		setOrderBy(property);
-	};
-
-	const handleSelectAllClick = (event) => {
-		if (event.target.checked) {
-			const newSelecteds = USERLIST.map((n) => n.name);
-			setSelected(newSelecteds);
-			return;
-		}
-		setSelected([]);
-	};
-
-	const handleClick = (event, name) => {
-		const selectedIndex = selected.indexOf(name);
-		let newSelected = [];
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, name);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1));
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1));
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1)
-			);
-		}
-		setSelected(newSelected);
-	};
-
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage);
-	};
-
-	const handleChangeRowsPerPage = (event) => {
-		setRowsPerPage(parseInt(event.target.value, 10));
-		setPage(0);
-	};
-
-	const handleFilterByName = (event) => {
-		setFilterName(event.target.value);
-	};
-
-	const emptyRows =
-		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
-
-	const filteredUsers = applySortFilter(
-		USERLIST,
-		getComparator(order, orderBy),
-		filterName
-	);
-
-	const isUserNotFound = filteredUsers.length === 0;
-
-
-	// const getData = async ()=>{
-	// 	try{
-	// 		const data = await getAllEmployees();
-	// 		console.log(data);
-	// 		if(data.status === 200 ){
-	// 			setEmployees(data?.data);
-	// 		}else{
-	// 			toast.error('Something went wrong!', {
-	// 				position: "top-right",
-	// 				autoClose: 5000,
-	// 				hideProgressBar: false,
-	// 				closeOnClick: true,
-	// 				pauseOnHover: true,
-	// 				draggable: true,
-	// 				progress: undefined,
-	// 			});
-	// 		} 
-	// 	}catch(error){
-	// 		console.log(error);
-	// 		toast.error('Something went wrong!', {
-	// 			position: "top-right",
-	// 			autoClose: 5000,
-	// 			hideProgressBar: false,
-	// 			closeOnClick: true,
-	// 			pauseOnHover: true,
-	// 			draggable: true,
-	// 			progress: undefined,
-	// 		});
-	// 	}
-	// }
-
-	// useEffect(() => {
-	// 	getData();
-	// }, []);
 
 
 	return (
@@ -216,75 +67,22 @@ export default function User() {
                 <Typography variant="h5" sx={{ py : 2}} align="center">
                     Gunmen's Monthly Attendance Record for Jan 2021
                 </Typography>
-				<Grid container spacing={3}>
-					<Grid item xs={12} sm={6} md={3}>
-						<AppWeeklySales />
-					</Grid>
-					<Grid item xs={12} sm={6} md={3}>
-						<AppNewUsers />
-					</Grid>
-					<Grid item xs={12} sm={6} md={3}>
-						<AppItemOrders />
-					</Grid>
-					<Grid item xs={12} sm={6} md={3}>
-						<AppBugReports />
-					</Grid>
-                </Grid>
-				<AddEmployee
-							isOpenFilter={isAddEmployeeOpen}
-							onOpenFilter= {()=>{setAddEmployeeOpen(true)}}
-							onCloseFilter={()=>{setAddEmployeeOpen(false)}}
-				/>
+				
 				<Card sx={{my:2}}>
-					<UserListToolbar
-						numSelected={selected.length}
-						filterName={filterName}
-						onFilterName={handleFilterByName}
-					/>
-
 					<Scrollbar >
 						<TableContainer sx={{ minWidth: 800 }}>
 							<Table>
 								<UserListHead
-									order={order}
-									orderBy={orderBy}
 									headLabel={TABLE_HEAD}
 									rowCount={USERLIST.length}
-									numSelected={selected.length}
-									onRequestSort={handleRequestSort}
-									onSelectAllClick={handleSelectAllClick}
 								/>
 								<TableBody>
 									
 								</TableBody>
-								{isUserNotFound && (
-									<TableBody>
-										<TableRow>
-											<TableCell
-												align="center"
-												colSpan={6}
-												sx={{ py: 3 }}
-											>
-												<SearchNotFound
-													searchQuery={filterName}
-												/>
-											</TableCell>
-										</TableRow>
-									</TableBody>
-								)}
+								
 							</Table>
 						</TableContainer>
 					</Scrollbar>
-
-					{/* <TablePagination
-						rowsPerPageOptions={[5, 10, 25]}
-						component="div"
-						count={USERLIST.length}
-						rowsPerPage={rowsPerPage}
-						page={page}
-						onPageChange={handleChangePage}
-						onRowsPerPageChange={handleChangeRowsPerPage}
-					/> */}
 				</Card>
                 <Grid container spacing={4} sx={{ py : 2}}>
                      <Grid item xs={12} md={12} lg={12}>
