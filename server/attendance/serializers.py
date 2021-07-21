a663e49cbe4d214db55a842508169ce9441ffc9c
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-21 12:48:34 +0530

    nm

--- server/attendance/serializers.py
+++ server/attendance/serializers.py
@@ -1,8 +1,4 @@
 from secrets import token_hex
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e727196521b08d5dd5108d86d85454683d05456
 from rest_framework import serializers
 from rest_framework.response import Response
 from users.serializers import BranchSerializer, UserSerializer
@@ -73,39 +69,6 @@ class AttendanceSerializer(serializers.ModelSerializer):
 
 
 class TripSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
-    # vehicle = RelatedFieldAlternative(
-    #     queryset=Vehicle.objects.all(), serializer=VehicleSerializer
-    # )
-    # custodian_1 = RelatedFieldAlternative(
-    #     queryset=Custodian.objects.all(), serializer=CustodianSerializer
-    # )
-    # custodian_2 = RelatedFieldAlternative(
-    #     queryset=Custodian.objects.all(), serializer=CustodianSerializer
-    # )
-    # custodian_3 = RelatedFieldAlternative(
-    #     queryset=Custodian.objects.all(), serializer=CustodianSerializer
-    # )
-    # branch = RelatedFieldAlternative(
-    #     queryset=Branch.objects.all(), serializer=BranchSerializer
-    # )
-    # added_by = RelatedFieldAlternative(
-    #     queryset=User.objects.all(), serializer=UserSerializer
-    # )
-    vehicle = VehicleSerializer(required=False)
-    custodian_1 = CustodianSerializer(required=False)
-    custodian_2 = CustodianSerializer(required=False)
-    custodian_3 = CustodianSerializer(required=False)
-    branch = BranchSerializer(required=False)
-    added_by = UserSerializer(required=False)
-
-    # vehicle = VehicleSerializer(required=False)
-    # custodian_1 =CustodianSerializer(required=False)
-    # custodian_2 = CustodianSerializer(required=False)
-    # custodian_3 = CustodianSerializer(required=False)
-    # branch = BranchSerializer(required=False)
-    # added_by = UserSerializer(required=False)
-=======
     vehicle = RelatedFieldAlternative(
         queryset=Vehicle.objects.all(), serializer=VehicleSerializer
     )
@@ -124,7 +87,6 @@ class TripSerializer(serializers.ModelSerializer):
     added_by = RelatedFieldAlternative(
         queryset=User.objects.all(), serializer=UserSerializer
     )
->>>>>>> 0e727196521b08d5dd5108d86d85454683d05456
 
     class Meta:
         model = Trip
@@ -148,38 +110,6 @@ class TripSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-<<<<<<< HEAD
-        validated_data["trip_code"] = token_hex(6).upper()
-
-        custodian_1 = validated_data.get("custodian_1")
-        custodian_2 = validated_data.get("custodian_2")
-        custodian_3 = validated_data.get("custodian_3")
-
-        if custodian_1 and custodian_2 and custodian_1 == custodian_2:
-            raise serializers.ValidationError(
-                {"error": "Same Custodian cannot be assigned twice"}
-            )
-        if custodian_1 and custodian_3 and custodian_1 == custodian_3:
-            raise serializers.ValidationError(
-                {"error": "Same Custodian cannot be assigned twice"}
-            )
-        if custodian_3 and custodian_2 and custodian_3 == custodian_2:
-            raise serializers.ValidationError(
-                {"error": "Same Custodian cannot be assigned twice"}
-            )
-
-        if custodian_1:
-            validated_data["custodian_1_code"] = token_hex(6).upper()
-        if custodian_2:
-            validated_data["custodian_2_code"] = token_hex(6).upper()
-        if custodian_3:
-            validated_data["custodian_3_code"] = token_hex(6).upper()
-
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-=======
->>>>>>> 0e727196521b08d5dd5108d86d85454683d05456
         print(validated_data)
         validated_data["trip_code"] = token_hex(6).upper()
 
@@ -189,12 +119,6 @@ class TripSerializer(serializers.ModelSerializer):
 
         if custodian_1:
             validated_data["custodian_1_code"] = token_hex(6).upper()
-<<<<<<< HEAD
-        if custodian_2:
-            validated_data["custodian_2_code"] = token_hex(6).upper()
-        if custodian_3:
-            validated_data["custodian_3_code"] = token_hex(6).upper()
-=======
         if custodian_2 != custodian_2:
             validated_data["custodian_2_code"] = token_hex(6).upper()
         if custodian_3 != custodian_1:
@@ -243,7 +167,6 @@ class TripSerializer(serializers.ModelSerializer):
                     custodian=custodian_3,
                     branch=instance.branch,
                 )
->>>>>>> 0e727196521b08d5dd5108d86d85454683d05456
 
         return super().update(instance, validated_data)
 
