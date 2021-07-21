ca2a9ef4f144ddddc98db42b1b4df86aa942fc58
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-21 22:36:47 +0530

    vehicle csv

--- server/attendance/cron.py
+++ server/attendance/cron.py
@@ -1,4 +1,4 @@
-from attendance.models import Attendance
+from attendance.models import Attendance, AttendanceVehicle
 from attendance.utils import qs_to_local_csv
 from django.conf import settings
 import os
@@ -8,13 +8,24 @@ BASE_DIR = settings.BASE_DIR
 
 
 def send_attendance_report():
-    qs = Attendance.objects.all()
+    qs1 = Attendance.objects.all()
+    qs2 = AttendanceVehicle.objects.all()
     qs_to_local_csv(
-        qs,
+        qs1,
         fields=[
             "id",
             "custodian__first_name",
             "custodian__last_name",
+            "entry_time",
+            "exit_time",
+        ],
+    )
+    qs_to_local_csv(
+        qs2,
+        fields=[
+            "id",
+            "vehicle__model_name",
+            "custodian__number_plate",
             "entry_time",
             "exit_time",
         ],
