22c9f04f2c92910934269df449a01c075a30923c
RUiNtheExtinct <deepkarma001@gmail.com>
2021-07-23 21:16:51 +0530

    nm

--- client/src/_mocks_/account.js
+++ client/src/_mocks_/account.js
@@ -1,10 +1,10 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoWomanURL: '/static/assets/images/woman.png',
-  photoManURL: '/static/assets/images/man.png'
+	displayName: "Jaydon Frankie",
+	email: "demo@minimals.cc",
+	photoWomanURL: "/static/assets/images/woman.png",
+	photoManURL: "/static/assets/images/man.png",
 };
 
 export default account;
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -14,7 +14,6 @@ const axiosInstance = axios.create({
 	},
 });
 
-
 axiosInstance.interceptors.response.use(
 	(response) => {
 		return response;
@@ -97,70 +96,79 @@ axiosInstance.interceptors.response.use(
 	}
 );
 
-// auth 
-export const signIn = (formData) => axiosInstance.post('token/', formData);
+// auth
+export const signIn = (formData) => axiosInstance.post("token/", formData);
 
 // employees (users)
-export const getAllEmployees = () => axiosInstance.get('users/user');
-export const addEmployee = (formData)=> axiosInstance.post('users/user/',formData);
+export const getAllEmployees = () => axiosInstance.get("users/user");
+export const addEmployee = (formData) =>
+	axiosInstance.post("users/user/", formData);
 
-// gunmen's attendance 
-export const viewAllAttendance = () => axiosInstance.get('attendance/attendance');
+// gunmen's attendance
+export const viewAllAttendance = () =>
+	axiosInstance.get("attendance/attendance");
 
 // gunmen
-export const addGunmen = (formData)=> axiosInstance.post('vendor/gunmen/',formData);
-export const updateGunmen = (formData)=> axiosInstance.put('vendor/gunmen/',formData);
-export const deleteGunmen = ()=> axiosInstance.delete('vendor/gunmen/');
-export const getGunmens = ()=> axiosInstance.get('vendor/gunmen');
+export const addGunmen = (formData) =>
+	axiosInstance.post("vendor/gunmen/", formData);
+export const updateGunmen = (formData) =>
+	axiosInstance.put("vendor/gunmen/", formData);
+export const deleteGunmen = () => axiosInstance.delete("vendor/gunmen/");
+export const getGunmens = () => axiosInstance.get("vendor/gunmen");
 
-// regions 
-export const getAllRegions = () => axiosInstance.get('users/region');
-export const addRegion = (formData) => axiosInstance.post('users/region/', formData);
-export const updateRegion = (id,formData) =>{
+// regions
+export const getAllRegions = () => axiosInstance.get("users/region");
+export const addRegion = (formData) =>
+	axiosInstance.post("users/region/", formData);
+export const updateRegion = (id, formData) => {
 	return axiosInstance.put(`users/region/${id}/`, formData);
-}
-export const deleteRegion = (id) =>{
+};
+export const deleteRegion = (id) => {
 	return axiosInstance.delete(`users/region/${id}/`);
-}
+};
 
-// vendors 
-export const getAllVendors = ()=> axiosInstance.get('vendor/vendor');
-export const addVendor = (formData) => axiosInstance.post('vendor/vendor/', formData);
-export const updateVendor = (id,formData) =>{
-	return axiosInstance.put (`vendor/vendor/${id}/`, formData);
-}
-export const deleteVendor = (row) =>{ console.log(row);
+// vendors
+export const getAllVendors = () => axiosInstance.get("vendor/vendor");
+export const addVendor = (formData) =>
+	axiosInstance.post("vendor/vendor/", formData);
+export const updateVendor = (id, formData) => {
+	return axiosInstance.put(`vendor/vendor/${id}/`, formData);
+};
+export const deleteVendor = (row) => {
+	console.log(row);
 	return axiosInstance.delete(`users/vendor/${row}/`);
-}
+};
 
-// Vehicle 
-export const getAllVehicles = () => axiosInstance.get('vendor/vehicle');
-export const AddVehicle = (formData) => axiosInstance.post('vendor/vehicle/', formData);
-export const updateVehicle = (id,formData) =>{
+// Vehicle
+export const getAllVehicles = () => axiosInstance.get("vendor/vehicle");
+export const AddVehicle = (formData) =>
+	axiosInstance.post("vendor/vehicle/", formData);
+export const updateVehicle = (id, formData) => {
 	return axiosInstance.put(`vendor/vehicle/${id}/`, formData);
-}
-export const deleteVehicle = (id) =>{
+};
+export const deleteVehicle = (id) => {
 	return axiosInstance.delete(`vendor/vehicle/${id}/`);
-}
+};
 
-// branch 
-export const getAllBranch = ()=> axiosInstance.get('users/branch');
-export const addBranch = (formData)=> axiosInstance.post('users/branch/', formData);
-export const updateBranch = (id,formData) =>{
+// branch
+export const getAllBranch = () => axiosInstance.get("users/branch");
+export const addBranch = (formData) =>
+	axiosInstance.post("users/branch/", formData);
+export const updateBranch = (id, formData) => {
 	return axiosInstance.put(`users/branch/${id}/`, formData);
-}
-export const deleteBranch = (id) =>{
+};
+export const deleteBranch = (id) => {
 	return axiosInstance.delete(`users/branch/${id}/`);
-}
-// Issues 
+};
+// Issues
 
 //Trip
-export const getTrip = ()=> axiosInstance.get('attendance/trip');
-export const addTrip = (formData)=> axiosInstance.post('attendance/trip/', formData);
-export const updateTrip= (id,formData) =>{
+export const getTrip = () => axiosInstance.get("attendance/trip");
+export const addTrip = (formData) =>
+	axiosInstance.post("attendance/trip/", formData);
+export const updateTrip = (id, formData) => {
 	return axiosInstance.put(`attendance/trip/${id}/`, formData);
-}
-export const deleteTrip = (id) =>{
+};
+export const deleteTrip = (id) => {
 	return axiosInstance.delete(`attendance/trip/${id}/`);
-}
-
+};
